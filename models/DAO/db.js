bc736dd9d49d9262a52ffeedcf798e81d6f51802
Luiza Mariana <luizamarianahg@hotmail.com>
2025-06-17 18:34:06 -0300

    soh por commitar mesmo rs beijos

--- models/DAO/db.js
+++ models/DAO/db.js
@@ -1,10 +1,18 @@
 const {Pool} = require("pg");
-
+/*
+    const pool = new Pool({
+        host: 'localhost',
+        port: 5432,
+        user: 'postgres',
+        password: 'Anac2004.',
+        database: 'db_ProjetoIntegradoNode',
+    });
+*/
 const pool = new Pool({
     host: 'localhost',
     port: 5432,
     user: 'postgres',
-    password: 'Anac2004.',
+    password: '',
     database: 'db_ProjetoIntegradoNode',
 });
 
